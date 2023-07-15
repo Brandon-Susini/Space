f6ed4470e8100dc2b9477fd41c6cd9c703380b2f
Brandon-Susini <brandonsusini@gmail.com>
2023-07-15 13:37:21 -0400

    First Commit

---
+++ PlanetResource.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name PlanetResource
+
+@export var rotation_speed: float
+@export var particle_velocity: float
+@export_enum("Sphere","Ring","Point") var emission_shape: String
+@export var orbit_distance: float
+@export var particle_color: Vector3
+@export var particle_spread: float
